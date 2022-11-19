4c8aab873d85ae8eb649ac7c5daa98a910b6db48
XQ.Z <zhuxianqiang@18.163.com>
2022-11-19 18:32:18 +0800

    22.11.19 安装clion,c++ hello world

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    for (int i = 0; i < 10; ++i) {
+        /**
+         * 循环10次
+         */
+        std::cout << "Hello, World!333" << std::endl;
+    }
+    return 0;
+}
