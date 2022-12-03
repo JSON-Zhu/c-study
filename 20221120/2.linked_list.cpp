7c60d592c020a656a2ce728a992eb99968d6b517
XQ.Z <zhuxianqiang@18.163.com>
2022-12-03 16:59:00 +0800

    22.12.3 更新头插法,尾插法,遍历,按值和按序号查找节点

--- 20221120/2.linked_list.cpp
+++ 20221120/2.linked_list.cpp
@@ -8,24 +8,126 @@ typedef struct LNode {
     struct LNode *next;
 } LNode, *LinkedList;
 
-bool initList(LinkedList &list);
-
-LinkedList list_headInsert(LinkedList &list);
-
-/**
- * 头插法
- * @param list
- * @return
- */
-LinkedList list_headInsert(LinkedList &list) {
-
-};
-
 /**
  * 初始化带头结点的链表
  * @param list
  * @return
  */
+
+bool initList(LinkedList &list);
+
+/**
+ * 头插法 常用于逆置链表
+ * @param list
+ * @return
+ */
+LinkedList list_headInsert(LinkedList &list);
+
+/**
+ * 尾插法
+ * @param list
+ * @return
+ */
+LinkedList list_tailInsert(LinkedList &list);
+
+/**
+ * 按序号查找节点
+ * @param i
+ * @return
+ */
+LNode *getElement(LinkedList list, int i);
+
+/**
+ * 按值查找表结点
+ * @param i
+ * @return
+ */
+LNode *getElementByValue(LinkedList list, int value) {
+    LNode *pNode = list->next;
+    while (pNode && pNode->data != value) {
+        pNode = pNode->next;
+    }
+    return pNode;
+};
+
+
+LNode *getElement(LinkedList list, int i) {
+    // 计数,初始化为1
+    int count = 1;
+    LNode *p = list->next;
+    if (i == 0) {
+        return list;
+    }
+    if (i < 1) {
+        return NULL;
+    }
+    while (p && count < i) {
+        p = p->next;
+        count++;
+    }
+    return p;
+}
+
+/**
+ * 遍历链表
+ * @param list
+ */
+void traverseList(LinkedList &list);
+
+LinkedList list_tailInsert(LinkedList &list) {
+    LNode *s;
+    int intData;
+    // 创建头结点
+    list = (LinkedList) malloc(sizeof(LNode));
+    // 初始化为空链表
+    list->next = NULL;
+    LNode *r = list;
+    scanf("%d", &intData);
+    while (intData != 9999) {
+        s = (LNode *) malloc(sizeof(LNode));
+        s->data = intData;
+        r->next = s;
+        // r指向新的表尾结点
+        r = s;
+        // 继续输入
+        scanf("%d", &intData);
+    }
+    // 输入结束,表尾结点指向null
+    r->next = NULL;
+    return list;
+};
+
+void traverseList(LinkedList &list) {
+    if (list == NULL)
+        return;
+    LNode *p = list->next;
+    while (p != NULL) {
+        cout << "data is: " << p->data << endl;
+        p = p->next;
+    }
+};
+
+
+LinkedList list_headInsert(LinkedList &list) {
+    LNode *s;
+    int intData;
+    // 创建头结点
+    list = (LinkedList) malloc(sizeof(LNode));
+    // 初始化为空链表
+    list->next = NULL;
+    scanf("%d", &intData);
+    while (intData != 9999) {
+        s = (LNode *) malloc(sizeof(LNode));
+        s->data = intData;
+        s->next = list->next;
+        list->next = s;
+        // 继续输入
+        scanf("%d", &intData);
+    }
+    return list;
+};
+
+
 bool initList(LinkedList &list) {
     list = (LNode *) malloc(sizeof(LNode));
     if (list == NULL) {
--- main22.cpp
+++ main22.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <stdlib.h>
 #include "20221120/1.dynamic_array_list.cpp"
+#include "20221120/2.linked_list.cpp"
+
 #define MaxSize 10
 
 //typedef struct  {
@@ -13,8 +15,11 @@ int main() {
     // 数组实现
     // mainTest();
     // 链表实现
-
-
+    LinkedList list;
+    list_tailInsert(list);
+    traverseList(list);
+    LNode *pNode = getElementByValue(list, 2);
+    cout << pNode << endl;
 
 
 //    seqList list;
